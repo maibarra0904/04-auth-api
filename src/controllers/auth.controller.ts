0fbe349745790ca375008e903b2bba07c7d1a432
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-08-05 09:41:57 -0500

    Creación de endpoint para hacer el login de usuario

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -30,3 +30,25 @@ export const register = async (req: Request, res: Response) => {
         throw new Error("Hubo un error al guardar el punto")
     }
 }
+
+export const login = async (req: Request, res: Response) => {
+
+    let {email, password} = req.body;
+    
+    try {
+
+        let user = await User.findOne({email})
+
+        if (!user) {
+            return res.status(400).json({error: "No existe usuario con ese email"})
+        }
+
+        if(!bcrypt.compareSync( password, user.password)) {
+            return res.status(400).json({error: "Contraseña incorrecta"})
+        }
+
+        return res.status(200).json({msg: "Usuario Loggeado correctamente"});
+    } catch (error) {
+        throw new Error("Hubo un error al iniciar sesión")
+    }
+}
